0a5fed5d7f011f24249fa09c18f2c52aa0b9f1f6
mush0000 <mashroom0825@gmail.com>
2023-11-25 14:30:41 +0900

    とりあえず動く

---
+++ Gold_fish/gold_fish.py
@@ -0,0 +1,43 @@
+from random import randint
+
+class gold_fish:
+    # life = 0  #ライフであり水質（水の透明度,白いフィルター）
+    # size = 10
+    # color = 10
+    # count = 16
+    # fish_die = False
+    def __init__(self):
+        self.life = 0  #ライフであり水質（水の透明度,白いフィルター）
+        self.size = 10
+        self.color = 10
+        self.count = 16
+        self.fish_die = False
+
+    def die_judge(self):
+            if(self.life >= 90):
+                self.fish_die = True
+
+    def counter(self):
+        #育成ターン経過
+        print("ターン経過前：{}".format(self.count))#デバック用
+        self.count -= 1
+        print("ターン経過：{}".format(self.count))#デバック用
+
+        #ターン経過時に水も汚れる
+        print("水質変化前：{}".format(self.life))#デバック用
+        self.life += randint(5,20)
+        print("水質変化後：{}".format(self.life))#デバック用
+        
+    def use_worm(self):
+        self.size += 6
+        gold_fish.counter(self)
+
+    def use_food(self):
+        self.color += 10
+        gold_fish.counter(self)
+
+    def use_water(self):
+        self.life -= 20
+        gold_fish.counter(self)
+
+    
--- Gold_fish/main.py
+++ Gold_fish/main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+import tkinter.ttk as ttk
+import gold_fish
 
-#1  タイトル画面表示(タイトルロゴ、スタートボタン、背景)
+#1  タイトル画面表示(タイトルロゴ、スタートボタン、背景)-------------------------------------
 root = tk.Tk()
 root.title("金魚の飼い方")
 root.geometry("500x500")
@@ -8,20 +10,30 @@ frame_width = 500
 frame_height = 500
 feame =tk.Frame(root,width=frame_width,height=frame_height)
 
-
+fish = gold_fish.gold_fish()
+#fish.counter() デバック用
 
 #画面を作る
 #ネットから引用(画面切り替え)
 #タイトル画面
 def show_screen_title():
     screen_title.pack()
-    screen_result.pack_forget()
+    # screen_result.pack_forget()
 #飼育画面
 def show_screen_breeding():
     screen_title.pack_forget()
     screen_breeding.pack()
-#結果画面
+    screen_breeding_button.pack()#育成コマンドを表示    
+    
+#結果画面(金魚死亡)
 def show_screen_result():
+    screen_breeding_button.pack_forget()#育成コマンドを非表示
+    screen_breeding.pack_forget()
+    screen_result.pack()
+
+#結果画面(育成終了)
+def show_screen_result():
+    screen_breeding_button.pack_forget()#育成コマンドを非表示
     screen_breeding.pack_forget()
     screen_result.pack()
     
@@ -41,74 +53,85 @@ button_title.pack(side=tk.BOTTOM)
 
 
 #   育成画面のフレーム-------------------------------------------------------------------------------
-screen_breeding = tk.Frame(root,width=frame_width,height=frame_height)
+#育成画面
+screen_breeding = tk.Frame(root,width=frame_width,height=frame_height)#背景
 screen_breeding.pack()
-screen_breeding.propagate(False)#←???
+screen_breeding.propagate(False)
 
 label_breeding = tk.Label(screen_breeding, text="ScreenBreeding")
 label_breeding.pack()
 
-#2  育成(ボタンで選択_エサA(虫),B(人口エサ),c(水替え))
+#行動選択ボタン用フレームキャンバス
+screen_breeding_button = tk.Frame(screen_breeding,bg="aquamarine",width=500,height=500)
+screen_breeding_button.pack(side="bottom")
+screen_breeding_button.propagate(False)
 
 
 
-#選択によってステータスが変化、カウントが進行
 
-#def use_worm():     # worm:金魚がでかくなるエサ(command=use_worm,)
+
+#2  育成(ボタンで選択_エサA(虫),B(人口エサ),c(水替え))--------------------------------------
+
+#A 金魚が大きく育つ# worm:金魚がでかくなるエサ
     #金魚の画像を2px大きくする
     #1ターン経過してループから外れる
+button_worm = tk.Button(screen_breeding_button, text="worm", width=20, height=5,command=fish.use_worm())
+button_worm.grid(row = 0,column=0)
+
+# #B  金魚の色が鮮やかになる# food:金魚の体色が鮮やかになるエサ
+#     # 金魚の色を鮮やかにする
+#     # 1ターン経過してループから外れる
+button_food = tk.Button(screen_breeding_button, text="food", width=20, height=5,command=fish.use_food())
+button_food.grid(row = 0,column=1, pady=(20))
 
 
-#def use_food():     # food:金魚の体色が鮮やかになるエサ(command=use_food,)
-    #金魚の色を鮮やかにする
-    #1ターン経過してループから外れる
-# button_food = tk.Button(screen_breeding, text="food", width=20, height=5)
-# button_food.grid(row = 500,column = 0,anchor="SW")
+# #C  水の濁りが解消    # water:金魚のlifeが回復する水
+# #     #lifeが20回復する
+# #     #1ターン経過してループから外れる
+button_water = tk.Button(screen_breeding_button, text="water", width=20, height=5,command=fish.use_water())
+button_water.grid(row = 0,column = 2)
 
-# #def use_water():     # water:金魚のlifeが回復する水(command=use_water,)
-#     #lifeが20回復する
-#     #1ターン経過してループから外れる
-# button_water = tk.Button(screen_breeding, text="water", width=20, height=5)
-# button_water.grid(row = 500,column = 0,anchor="SW")
 
-#C  水の濁りが解消
 
-#飼育ターンを使用(経過で水が濁る80超えると金魚死亡)
+#3  結果画面表示(ss~Fまでの評価結果,金額表示)
+#ゲーム終了判定------------------------------------------------------------------------------
 
-# #ゲーム終了判定
-# if(fish_life <= 0):
-# #   show_screen_resurt()
-#     screen_breeding.propagate(False)
-# elif(fish_time ==0):
-#     screen_breeding.propagate(False)
+fish.die_judge()
+judge = fish.die_judge()
+#judge = True   #デバック用
+if (judge == True):     #金魚死亡を判定、死んでいたら『リザルト：死亡』へ
+    #ScreenResurtボタンが表示され、クリックで結果画面に切り替える
+    button_breeding = tk.Button(screen_breeding, text="Result", command=show_screen_result,width=30, height=5)
+    button_breeding.pack(side=tk.BOTTOM)
 
-#3  飼育ターンが0または金魚が死ぬと、結果画面表示(ss~Fまでの評価結果,金額表示)
-#ScreenResurtボタンクリックで結果画面に切り替える
-# button_breeding = tk.Button(screen_breeding, text="Result", command=show_screen_result,width=30, height=5)
-# button_breeding.pack(side=tk.BOTTOM)
+    screen_result = tk.Frame(root,width=frame_width,height=frame_height)
+    screen_result.pack()
+    screen_result.propagate(False)
 
-# button_worm = tk.Button(screen_breeding, text="worm", width=20, height=5,anchor="sw")
-# button_worm.grid(column=0,padx=(0), pady=(100))
+    #S結果画面に切り替える
+    label_result = tk.Label(screen_result, text="ScreenResult")
+    label_result.pack()
 
-button_food = tk.Button(screen_breeding, text="food", width=20, height=5)
-button_food.pack(side = "bottom")
+    button_breeding = tk.Button(screen_result, text="title", command=show_screen_title,width=30, height=5)
+    button_breeding.pack(side=tk.BOTTOM)
 
-button_water = tk.Button(screen_breeding, text="water", width=20, height=5)
-button_water.pack(side = "bottom")
+if(fish.count == 0):    #残りターン数字を判定、『リザルト：通常』へ
+    #ScreenResurtボタンが表示され、クリックで結果画面に切り替える
+    button_breeding = tk.Button(screen_breeding, text="Result", command=show_screen_result,width=30, height=5)
+    button_breeding.pack(side=tk.BOTTOM)
 
-#4  リザルト画面
-#   結果画面のフレーム-------------------------------------------------------------------------------
-screen_result = tk.Frame(root,width=frame_width,height=frame_height)
-screen_result.pack()
-screen_result.propagate(False)
+    screen_result = tk.Frame(root,width=frame_width,height=frame_height)
+    screen_result.pack()
+    screen_result.propagate(False)
 
-#ScreenTitleボタンクリックで結果画面に切り替える
-label_result = tk.Label(screen_result, text="ScreenResult")
-label_result.pack()
+    #ScreenTitleボタンクリックで結果画面に切り替える
+    label_result = tk.Label(screen_result, text="ScreenResult")
+    label_result.pack()
 
-button_breeding = tk.Button(screen_result, text="title", command=show_screen_title,width=30, height=5)
-button_breeding.pack(side=tk.BOTTOM)
+    button_breeding = tk.Button(screen_result, text="title", command=show_screen_title,width=30, height=5)
+    button_breeding.pack(side=tk.BOTTOM)
 
+#4  リザルト画面-------------------------------------------------------------------------------
 # 最初はタイトル画面を表示
 show_screen_title()
 
